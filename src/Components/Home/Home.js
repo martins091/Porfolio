732484b26679c3d5f6ae07752d26b801059eaa39
tochukwu <tochukwu.victoruk@gmail.com>
2025-12-11 12:54:22 +0100

    added some updates

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,52 +2,55 @@ import React from "react";
 import "./Home.css";
 import martins from "../../assets/images/martins.svg";
 
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="home-text" data-aos="fade-down">
+        <strong>Hello, I'm</strong>
+        <h1>Martins Ejiofor</h1>
+        <p>
+          I am a Software Engineer, innovator, and leader with a passion for
+          turning ideas into impactful digital experiences. With over four years
+          of experience in Full-Stack Development, Frontend &
+          Backend Development, and WordPress, I build solutions that are not
+          only functional but also intuitive and user-centered. As a leader and
+          mentor, I inspire teams to reach their full potential, fostering
+          creativity, collaboration, and growth. My journey—from humble
+          beginnings to building my own tech company—drives me to empower others
+          to pursue their passions and achieve their dreams through technology.
+          I am committed to creating meaningful, scalable solutions that make a
+          difference, while inspiring and guiding the next generation of tech
+          innovators.
+        </p>
 
-const Home = ()=> {
-    return(
-       <section id="home">
-        <div className="home-text" data-aos="fade-down">
-            <strong>Hello, I'm</strong>
-            <h1>Martins Ejiofor</h1>
-            <p> I am a seasoned FullStack Developer with over three years of experience, 
-                dedicated to crafting seamless and visually captivating digital experiences. 
-                Proficient in Frontend Development, Backend Development, Mern-Stack, Wordpress  etc. 
-                I excel in transforming innovative design concepts into interactive and 
-                user-friendly interfaces. My deep understanding of user-centered design 
-                principles and my ability to bridge the gap between aesthetics and functionality 
-                have consistently resulted in engaging and intuitive web applications. 
-                With a proven track record of collaborating effectively with multidisciplinary 
-                teams and a constant drive to refine my skills, I am committed to delivering impactful 
-                and responsive solutions that elevate user engagement and satisfaction.
-            </p>
-            <a href="#summery" className="btn-link">Scroll Down</a>
-
+        <a href="#summery" className="btn-link">
+          Scroll Down
+        </a>
+      </div>
+      <div className="home-img" data-aos="fade-up">
+        <div className="img-box">
+          <img src={martins} alt="martins" />
+          <h2>
+            Martins Ejiofor <br /> <br />
+            <span> FullStack Developer </span>
+          </h2>
+          <div className="social">
+            <a href="https://www.facebook.com/profile.php?id=100086715874671">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/martins-ejiofor-a9b32b26a/">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/martins091">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+          <a href="mailto:agbomartinsejifor@gmail.com" className="hire-me">
+            Hire me
+          </a>
         </div>
-        <div className="home-img" data-aos="fade-up">
-            <div className="img-box">
-                <img src={martins} alt="martins"/>
-                <h2>Martins Ejiofor <br/> <br/><span> FullStack Developer </span></h2>
-                <div className="social">
-                    <a href="https://www.facebook.com/profile.php?id=100086715874671">
-                    <i className="fa-brands fa-facebook"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/martins-ejiofor-a9b32b26a/">
-                    <i className="fa-brands fa-linkedin"></i>
-                    </a>
-                    <a href="https://github.com/martins091">
-                    <i className="fa-brands fa-github"></i>
-                    </a>
-
-                </div>
-                <a href="mailto:agbomartinsejifor@gmail.com" className="hire-me">Hire me</a>
-
-            </div>
-            
-        </div>
-
-       </section>
-
-
-    )
-}
+      </div>
+    </section>
+  );
+};
 export default Home;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,56 +1,49 @@
-
-import React from 'react';
-
-
+import React from "react";
 
 import "./Projects.css";
 import Project1 from "../../assets/images/project1.jpeg";
-import Project2 from "../../assets/images/project2.jpg";
+import Project2 from "../../assets/images/project2.jpeg";
 import Project3 from "../../assets/images/project3.jpeg";
-import Project4 from "../../assets/images/project4.jpeg";
+// import Project4 from "../../assets/images/project4.jpeg";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // import './styles.css';
 
 // import required modules
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from "swiper/modules";
 
+const Projects = () => {
+  const data = [
+    {
+      src: Project1,
+      url: "https://nigeria.lushhairafrica.com/",
+    },
+    {
+      src: Project2,
+      url: "https://app-sodc.optimusai.ai/",
+    },
+    {
+      src: Project3,
+      url: "https://tastes-of-culture.vercel.app/",
+    },
+  ];
+  return (
+    <section id="projects">
+      <div className="project-heading">
+        <h3>Recent Projects</h3>
+        <p>
+          *Some are group project while some are personal project, click on it
+          to take you to the website*
+        </p>
+      </div>
 
-const Projects = () =>{
-
-    const data = [
-        {
-            src: Project1,
-            url: "https://app.finezcv.com/"
-        },
-        {
-            src: Project2,
-            url: "https://mtn-sim-app-git-martins-babalolad-tezzas-projects.vercel.app/"
-        },
-        {
-            src: Project3,
-            url: "https://mern-blog-rja7.onrender.com"
-        },
-        {
-            src: Project4,
-            url: "https://realestate-app-ymhb.onrender.com"
-        },
-    ]
-    return(
-        <section id="projects">
-            <div className="project-heading">
-                <h3>Recent Projects</h3>
-                <p>*Some are group project while some are personal project, click on it to take you to the website*</p>
-                
-            </div>
-
-            <Swiper
+      <Swiper
         slidesPerView={1}
         spaceBetween={10}
         navigation={true}
@@ -62,50 +55,38 @@ const Projects = () =>{
             slidesPerView: 2,
             spaceBetween: 20,
           },
-        //   768: {
-        //     slidesPerView: 4,
-        //     spaceBetween: 40,
-        //   },
-        //   1024: {
-        //     slidesPerView: 5,
-        //     spaceBetween: 50,
-        //   },
+          //   768: {
+          //     slidesPerView: 4,
+          //     spaceBetween: 40,
+          //   },
+          //   1024: {
+          //     slidesPerView: 5,
+          //     spaceBetween: 50,
+          //   },
         }}
         modules={[Navigation, Pagination]}
         className="mySwiper"
       >
-        {
-            data.map(project => (
-                <SwiperSlide key={project.src}>
-                    <div className="project-box">
-                        <a href={project.url}>
-                            <img src={project.src} alt="project"/>
-                            <div className="p-overlayer">
-                                <strong>Outserved Reports</strong>
-
-                            </div>
-
-                        </a>
-
-                    </div>
-                    
-                </SwiperSlide>
-
-            ))
-        }
-        
-       
+        {data.map((project) => (
+          <SwiperSlide key={project.src}>
+            <div className="project-box">
+              <a href={project.url} target="_blank" rel="noreferrer">
+                <img src={project.src} alt="project" />
+                <div className="p-overlayer">
+                  <strong>web application that is serving people</strong>
+                </div>
+              </a>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
 
-
-            <div className="github-link">
-                <a href="https://github.com/martins091" className="btn-link">
-                My Github <i className="fa-brands fa-github"></i>
-                </a>
-
-            </div>
-            
-        </section>
-    )
-}
-export default Projects; 
+      <div className="github-link">
+        <a href="https://github.com/martins091" className="btn-link">
+          My Github <i className="fa-brands fa-github"></i>
+        </a>
+      </div>
+    </section>
+  );
+};
+export default Projects;
